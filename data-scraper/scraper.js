4d2d8783d80f930eaa7be65300b41226c062babe
Anita Lipsky <anita@purplebugs.com>
2019-12-17 08:56:56 +0100

    Data scraper gets title of Wikipedia page

--- data-scraper/scraper.js
+++ data-scraper/scraper.js
@@ -1,6 +1,23 @@
-// Using module from npm
-const jsdom = require("jsdom");
+// Using modules from npm
+const fetch = require('node-fetch')
+const jsdom = require('jsdom')
+
+const url = 'https://en.wikipedia.org/wiki/List_of_Eurovision_Song_Contest_winners';
 const { JSDOM } = jsdom;
 
-const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
-console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
+fetch(url)
+  .then(response => {
+    return response.text() // Get plain text from the response
+  })
+  .then(text => {
+    // This plain text is HTML which JSDOM will map to a DOM
+    const dom = new jsdom.JSDOM(text);
+
+    // The DOM has a .window property which has a .document property, similiar to how it is in a browser
+    return dom.window.document;
+  })
+  .then(document => {
+    // Get the title of the document
+    const title = document.title;
+    console.log('TITLE:', title);
+    })
