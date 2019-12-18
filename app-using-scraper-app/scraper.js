a032b55c3d04038f4ce6d53bf741b7241a735b08
Anita Lipsky <anita@purplebugs.com>
2019-12-18 15:14:33 +0100

    New folder for app that now uses scraper app

---
+++ app-using-scraper-app/scraper.js
@@ -0,0 +1,110 @@
+// Using modules from npm
+const fetch = require('node-fetch')
+const jsdom = require('jsdom')
+
+const url = 'https://en.wikipedia.org/wiki/List_of_Eurovision_Song_Contest_winners';
+
+const winners = [
+
+]
+
+const handle1956 = function(elements){
+  console.log('Need to handle 1956 differently since there were no points, margins or runners up. TODO')
+  //console.log(elements[0].innerHTML)
+  return {name:"1956 TO DO"}
+}
+
+const handle1969 = function(elements){
+  console.log('Need to handle 1969 differently since there were 4 winners. TODO')
+  //console.log(elements[0].innerHTML)
+  return {name:"1969 TO DO"}
+}
+
+const handle2020 = function(elements){
+  console.log('Need to handle 2020 differently since the competition has not started. TODO')
+  //console.log(elements[0].innerHTML)
+  return {name:"2020 TO DO"}
+}
+
+function start() {
+  fetch(url)
+    .then(response => {
+      return response.text() // Get plain text from the response
+    })
+    .then(text => {
+      // This plain text is HTML which JSDOM will map to a DOM
+      const dom = new jsdom.JSDOM(text);
+
+      // The DOM has a .window property which has a .document property, similiar to how it is in a browser
+      return dom.window.document;
+    })
+    .then(document => {
+      
+      // Get the table
+      const winnersTableRows = document.querySelectorAll(".wikitable.sortable.plainrowheaders > tbody > tr") // If only specify .wikitable this picks up more than one table
+
+      // Get the rows
+      winnersTableRows.forEach(function(row) {
+
+          // Get year from current row
+          const yearElement = row.querySelectorAll("th a")[0];
+          const restOfElements = row.querySelectorAll("td");
+          let thisYearsWinner;
+
+          if (yearElement) {
+            const year = yearElement.textContent
+            switch (year) {
+              case "1956" :  
+                thisYearsWinner = handle1956(restOfElements)
+                winners.push({year: thisYearsWinner})
+                break
+              case "1969" : 
+                thisYearsWinner = handle1969(restOfElements)
+                winners.push({year: thisYearsWinner})
+                break
+              case "2020" :
+                thisYearsWinner = handle2020(restOfElements)
+                winners.push({year: thisYearsWinner})
+              break
+
+              default:
+
+              let language = ""
+
+              if (restOfElements[0] && restOfElements[1] && restOfElements[2] && restOfElements[3]) { // Check none are undefined
+                if (restOfElements[1].querySelectorAll("a")[1] &&
+                      restOfElements[1].querySelectorAll("a")[1].innerHTML &&
+                        restOfElements[2].querySelectorAll("a")[0].innerHTML &&
+                          restOfElements[3].querySelectorAll("a")[0].innerHTML &&
+                            restOfElements[4].querySelectorAll("a")[0].innerHTML) { // Check not undefined
+
+                  // // Some table cells start directly with the language, some with a link to the language, some inside ""
+                  // if (restOfElements[5]) {
+                  //   language = restOfElements[5].innerHTML // TODO parse to get first instance of language by snipping "" and search after >
+                  // } 
+
+                winners.push({
+                  year: year,
+                  date: restOfElements[0].innerHTML.trim(),
+                  hostCity: restOfElements[1].querySelectorAll("a")[1].innerHTML,
+                  winner: restOfElements[2].querySelectorAll("a")[0].innerHTML,
+                  song: restOfElements[3].querySelectorAll("a")[0].innerHTML,
+                  performer: restOfElements[4].querySelectorAll("a")[0].innerHTML,
+                  // language: language,
+
+                  // Work in progress
+                })
+              }
+              }
+            }
+          }
+
+      })
+      
+      console.log('Winners: ', winners)
+      return winners
+
+    })
+  }
+
+exports.start = start;
---
+++ app-using-scraper-app/server.js
@@ -0,0 +1,44 @@
+// Using modules from npm
+const fetch = require("node-fetch");
+const express = require("express");
+
+// Using custom app
+const scraper = require("./scraper");
+
+// Constants
+PORT = 8080;
+HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+// Create node web server
+app.get('/', (req, res) => { 
+
+// Use promise all construct to run requests synchronously
+Promise.all([
+    fetch("http://es01:9200").then((fetchResponses) => {return fetchResponses.json()}), // To be able to access json object properties
+    fetch("http://es01:9200/_cat/indices?v").then((fetchResponses) => {return fetchResponses.text()})
+]).then((bodies) => {
+    res.send(
+        bodies[0].name + // Able to access json object properties
+        bodies[1] + "Test123" + scraper.start() ) // This is text TODO + scraper.start() to try out data scraper
+}).catch((error) => {res.send(error)})
+
+});
+
+app.listen(PORT, HOST);
+
+// Note: This output is only useful if not running inside a docker container, since this console is not seen from outside the container
+console.log(`Running on http://${HOST}:${PORT}`);
+console.log('Navigate to http://0.0.0.0:8080/ to see info about the Elasticsearch cluster running');
+
+/*
+
+TODO: Test elasticesearch is running: curl -X GET "localhost:9200/_cat/nodes?v&pretty"
+
+Expected response:
+ip         heap.percent ram.percent cpu load_1m load_5m load_15m node.role master name
+172.27.0.2           23          62   1    0.34    0.24     0.18 dilm      *      es01
+
+*/
