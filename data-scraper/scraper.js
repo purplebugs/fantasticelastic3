b253974458697679a8868e182619124ca83889ba
Anita Lipsky <anita@purplebugs.com>
2019-12-18 09:33:14 +0100

    Dummy functions for non conforming years, clean up whitespace, consistently use innerHTML, add song and performer

--- data-scraper/scraper.js
+++ data-scraper/scraper.js
@@ -9,10 +9,22 @@ const winners = [
 
 ]
 
+const handle1956 = function(elements){
+  console.log('Need to handle 1956 differently since there were no points, margins or runners up. TODO')
+  //console.log(elements[0].innerHTML)
+  return {name:"1956 TO DO"}
+}
+
 const handle1969 = function(elements){
   console.log('Need to handle 1969 differently since there were 4 winners. TODO')
   //console.log(elements[0].innerHTML)
-  return {name:"foo"}
+  return {name:"1969 TO DO"}
+}
+
+const handle2020 = function(elements){
+  console.log('Need to handle 2020 differently since the competition has not started. TODO')
+  //console.log(elements[0].innerHTML)
+  return {name:"2020 TO DO"}
 }
 
 fetch(url)
@@ -41,29 +53,46 @@ fetch(url)
 
         // Get year from current row
         
-        const yearElement = row.querySelectorAll("th a")[0]
-        const restOfElements = row.querySelectorAll("td")
-        let thisYearsWinner
+        const yearElement = row.querySelectorAll("th a")[0];
+        const restOfElements = row.querySelectorAll("td");
+        let thisYearsWinner;
 
         if (yearElement) {
           const year = yearElement.textContent
           switch (year) {
-            case "1969" :  
-            thisYearsWinner = handle1969(restOfElements)
-            winners.push({year: thisYearsWinner})
+            case "1956" :  
+              thisYearsWinner = handle1956(restOfElements)
+              winners.push({year: thisYearsWinner})
               break
+            case "1969" : 
+              thisYearsWinner = handle1969(restOfElements)
+              winners.push({year: thisYearsWinner})
+              break
+            case "2020" :
+              thisYearsWinner = handle2020(restOfElements)
+              winners.push({year: thisYearsWinner})
+            break
+
             default:
 
-            if (restOfElements[0] && restOfElements[1] && restOfElements[2]) {
+            if (restOfElements[0] && restOfElements[1] && restOfElements[2] && restOfElements[3]) { // Check none are undefined
+              if (restOfElements[1].querySelectorAll("a")[1] &&
+                    restOfElements[1].querySelectorAll("a")[1].innerHTML &&
+                      restOfElements[2].querySelectorAll("a")[0].innerHTML &&
+                        restOfElements[3].querySelectorAll("a")[0].innerHTML &&
+                          restOfElements[4].querySelectorAll("a")[0].innerHTML) { // Check not undefined
 
               winners.push({
                 year: year,
-                date: restOfElements[0].innerHTML,
-                hostCity: restOfElements[1].innerHTML,
-                winner: restOfElements[2].innerHTML
+                date: restOfElements[0].innerHTML.trim(),
+                hostCity: restOfElements[1].querySelectorAll("a")[1].innerHTML,
+                winner: restOfElements[2].querySelectorAll("a")[0].innerHTML,
+                song: restOfElements[3].querySelectorAll("a")[0].innerHTML,
+                performer: restOfElements[4].querySelectorAll("a")[0].innerHTML,
 
                 // Work in progress
               })
+             }
             }
           }
         }
